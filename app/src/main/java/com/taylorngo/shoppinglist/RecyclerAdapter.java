863426fbfd62649f17843bf058db500775b8dea5
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-06-01 00:10:07 -0400

    added toggle details

--- app/src/main/java/com/taylorngo/shoppinglist/RecyclerAdapter.java
+++ app/src/main/java/com/taylorngo/shoppinglist/RecyclerAdapter.java
@@ -9,9 +9,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
@@ -30,6 +32,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
         private TextView descriptionTxt;
         private ImageView itemImage;
         private CheckBox purchasedBox;
+        private ToggleButton toggleDetails;
         private ConstraintLayout mainLayout;
 
         public MyViewHolder(final View view){
@@ -39,6 +42,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
             descriptionTxt = view.findViewById(R.id.descriptionText);
             itemImage = view.findViewById(R.id.itemImage);
             purchasedBox = view.findViewById(R.id.purchasedBox);
+            toggleDetails = view.findViewById(R.id.detailsBtn);
             mainLayout = view.findViewById(R.id.list_item_row);
         }
     }
@@ -62,6 +66,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
         String description = item.getDescription();
         holder.descriptionTxt.setText(description);
+        holder.descriptionTxt.setVisibility(View.INVISIBLE);
 
         String category = item.getCategory();
         if(category.equals("Food")){
@@ -78,6 +83,20 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
         }
 
         holder.purchasedBox.setChecked(item.isPurchased());
+
+        holder.toggleDetails.setTextOff("Show details");
+        holder.toggleDetails.setTextOn("Hide details");
+        holder.toggleDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(holder.toggleDetails.isChecked()){
+                    holder.descriptionTxt.setVisibility(View.VISIBLE);
+                }
+                else{
+                    holder.descriptionTxt.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
         holder.mainLayout.setOnClickListener(new View.OnClickListener() {
             @Override
